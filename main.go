307f653de78ad07431e1da101a7403b4cd78945c
GaoYang <GaoYangBenYang@outlook.com>
2022-03-06 19:03:18 +0800

    解决CORS问题

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/cors"
 	_ "resbox_go/database"
 	_ "resbox_go/routers"
 )
@@ -11,6 +12,22 @@ func main() {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+
+	//InsertFilter是提供一个过滤函数解决cors问题
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		//允许访问所有源
+		AllowAllOrigins: true,
+		//可选参数"GET", "POST", "PUT", "DELETE", "OPTIONS" (*为所有)
+		//其中Options跨域复杂请求预检
+		AllowMethods:   []string{"*"},
+		//指的是允许的Header的种类
+		AllowHeaders: 	[]string{"*"},
+		//公开的HTTP标头列表
+		ExposeHeaders:	[]string{"Content-Length"},
+		//如果设置，则允许共享身份验证凭据，例如cookie
+		AllowCredentials: true,
+	}))
+
 	//启动服务
 	beego.Run()
 }
